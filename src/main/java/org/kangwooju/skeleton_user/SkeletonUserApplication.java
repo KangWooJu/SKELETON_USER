dd53e452b814060aa1e6fffd593fabce59f10237
KangWooJu <space1102@naver.com>
2025-05-30 14:54:24 +0900

    feat:default package

---
+++ src/main/java/org/kangwooju/skeleton_user/SkeletonUserApplication.java
@@ -0,0 +1,13 @@
+package org.kangwooju.skeleton_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkeletonUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kangwooju/skeleton_user/SkeletonUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kangwooju.skeleton_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
