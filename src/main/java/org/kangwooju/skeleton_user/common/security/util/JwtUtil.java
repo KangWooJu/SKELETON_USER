c76a2f7e0955aa679b86178b91871eab024735e5
KangWooJu <space1102@naver.com>
2025-06-03 17:23:20 +0900

    refactor : 코드 가시요소 변경

--- src/main/java/org/kangwooju/skeleton_user/common/security/util/JwtUtil.java
+++ src/main/java/org/kangwooju/skeleton_user/common/security/util/JwtUtil.java
@@ -15,11 +15,15 @@ public class JwtUtil {
 
     private SecretKey secretKey;
 
-
-    public JwtUtil(@Value("${spring.jwt.secret}")String secret){
+    public JwtUtil(@Value("${jwt.secret}")String secret){
 
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
-                                      Jwts.SIG.HS256.key().build().getAlgorithm());
+                                      Jwts
+                                              .SIG
+                                              .HS256
+                                              .key()
+                                              .build()
+                                              .getAlgorithm());
 
     }
 
