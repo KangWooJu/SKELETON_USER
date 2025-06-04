faaa0ecb201dc7298e4425422178dd95fe8ffa2d
KangWooJu <space1102@naver.com>
2025-06-04 13:59:19 +0900

    refactor : SecurityConfig에 JWTFilter 타입의 객체필드 추가 및 빈 등록

--- src/main/java/org/kangwooju/skeleton_user/common/config/SecurityConfig.java
+++ src/main/java/org/kangwooju/skeleton_user/common/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package org.kangwooju.skeleton_user.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.kangwooju.skeleton_user.common.security.filter.JWTFilter;
 import org.kangwooju.skeleton_user.common.security.filter.LoginFilter;
 import org.kangwooju.skeleton_user.common.security.util.JwtUtil;
+import org.kangwooju.skeleton_user.domain.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +31,14 @@ public class SecurityConfig {
     private final ObjectMapper objectMapper;
     private final JwtUtil jwtUtil;
     private final LoginFilter loginFilter;
+    private final JWTFilter jwtFilter;
+    private final UserRepository userRepository;
+
+    @Bean
+    public JWTFilter jwtFilter(JwtUtil jwtUtil,UserRepository userRepository){
+        return new JWTFilter(jwtUtil,userRepository);
+    }
+
 
     // LoginFilter를 Config에서 bean으로 등록
     @Bean
@@ -68,7 +78,9 @@ public class SecurityConfig {
 
         httpSecurity
                 .addFilterAt(loginFilter,
-                        UsernamePasswordAuthenticationFilter.class);
+                        UsernamePasswordAuthenticationFilter.class); // 필터 순서 2
+        httpSecurity
+                .addFilterBefore(jwtFilter, LoginFilter.class); // 필터 순서 1
         // 세션을 유지하지 않도록 하는 설정 -> STATELESS
         httpSecurity
                 .sessionManagement((session)->session
