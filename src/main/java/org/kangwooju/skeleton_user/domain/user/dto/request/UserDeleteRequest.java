6ccb277268a2a313d2eb2fcba043a0047ac29e9e
KangWooJu <space1102@naver.com>
2025-06-04 13:59:39 +0900

    feat : 회원 삭제 로직구현을 위한 DTO 추가

---
+++ src/main/java/org/kangwooju/skeleton_user/domain/user/dto/request/UserDeleteRequest.java
@@ -0,0 +1,5 @@
+package org.kangwooju.skeleton_user.domain.user.dto.request;
+
+public record UserDeleteRequest(String token) {
+
+}
---
+++ src/main/java/org/kangwooju/skeleton_user/domain/user/dto/response/UserDeleteResponse.java
@@ -0,0 +1,4 @@
+package org.kangwooju.skeleton_user.domain.user.dto.response;
+
+public record UserDeleteResponse(String username) {
+}
