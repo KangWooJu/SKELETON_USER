7ba21998ecae38283fd9bfc23dfb9ff9cb0d874a
KangWooJu <space1102@naver.com>
2025-06-19 15:02:06 +0900

    feat : Api전용 성공 response생성

--- src/main/java/org/kangwooju/skeleton_user/common/dto/response/ApiSuccessResponse.java
+++ src/main/java/org/kangwooju/skeleton_user/common/dto/response/ApiSuccessResponse.java
@@ -1,7 +1,5 @@
 package org.kangwooju.skeleton_user.common.dto.response;
 
-public record ApiSuccessResponse(boolean success,
-                                 String method,
-                                 String message,
-                                 String timestamp) {
+public record ApiSuccessResponse(String meesage,
+                                 String time) {
 }
