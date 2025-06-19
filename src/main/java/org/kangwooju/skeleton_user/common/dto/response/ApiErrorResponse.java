6e33d2a829445e624c9268faf2636bd5d34e7324
KangWooJu <space1102@naver.com>
2025-06-19 14:46:23 +0900

    refactor : 클래스명 변경 CustomErrorResponse -> ApiErrorResponse

--- src/main/java/org/kangwooju/skeleton_user/common/dto/response/ApiErrorResponse.java
+++ src/main/java/org/kangwooju/skeleton_user/common/dto/response/ApiErrorResponse.java
@@ -1,7 +1,35 @@
 package org.kangwooju.skeleton_user.common.dto.response;
 
-public record ApiErrorResponse(boolean error,
-                               String method,
-                               String message,
-                               String timestamp) {
+import lombok.Builder;
+import lombok.Getter;
+import org.kangwooju.skeleton_user.common.exception.CustomException;
+import org.kangwooju.skeleton_user.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+@Builder
+@Getter
+public class ApiErrorResponse {
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    public ApiErrorResponse(ErrorCode errorCode){
+        this.httpStatus = errorCode.getHttpStatus();
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+
+    public static ResponseEntity<ApiErrorResponse> error(CustomException e){
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus()) // status 설정
+                .body(ApiErrorResponse // body 설정
+                        .builder()
+                        .httpStatus(e.getErrorCode().getHttpStatus())
+                        .code(e.getErrorCode().name())
+                        .message(e.getErrorCode().getMessage())
+                        .build());
+    }
 }
